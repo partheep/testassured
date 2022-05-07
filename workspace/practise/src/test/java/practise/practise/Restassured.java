fad6200638105c54b1805bfc9be1431fd5caae31
partheepbommina <partheepbommina@gmail.com>
2022-05-07 17:15:18 +0530

    latestcode is added for webdriver script

--- workspace/practise/src/test/java/practise/practise/Restassured.java
+++ workspace/practise/src/test/java/practise/practise/Restassured.java
@@ -1,8 +1,16 @@
 package practise.practise;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Restassured {
+	WebDriver driver;
+	@BeforeTest
+	public void  entertintappliacyion()
+	{
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+	}
   @Test
   public void f() {
   }
