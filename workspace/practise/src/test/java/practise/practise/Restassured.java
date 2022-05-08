e2ab606e7c5bc0c01550369fcca98d9e8f6d25d5
partheepbommina <partheepbommina@gmail.com>
2022-05-08 12:43:34 +0530

    code is completed

--- workspace/practise/src/test/java/practise/practise/Restassured.java
+++ workspace/practise/src/test/java/practise/practise/Restassured.java
@@ -1,6 +1,7 @@
 package practise.practise;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -11,11 +12,27 @@ public class Restassured {
   public void launch()
   {
     System.setProperty("webdriver.chrome.driver","E://chromedriver.exe");
-    driver.get("");
+    driver.get("https://google.com");
     driver.manage().window().maximize();
   }
   
   @Test
-  public void f() {
+  public void vlidate() {
+	  
+	  String Expected=driver.getTitle();
+	  String actual=Expected;
+	  if(actual.contains(Expected))
+	  {
+		  System.out.println("test case validated");
+	  }
+	  else
+	  {
+		  System.out.println("not passed the values");
+	  }
+  }
+  @AfterTest
+  public void teardown()
+  {
+	  driver.close();
   }
 }
